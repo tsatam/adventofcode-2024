5155cd1f71c08860d1716f4537c2c10fcdcda5df
Tanmay Satam <tsatam93@gmail.com>
2024-12-04 08:15:58 -0500

    Day 04 Part 1

---
+++ day/04/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+
+	"github.com/tsatam/adventofcode-2024/common/cartesian"
+	"github.com/tsatam/adventofcode-2024/common/fp"
+)
+
+var (
+	//go:embed input
+	input    string
+	wantWord = []rune("XMAS")
+)
+
+func main() {
+	fmt.Printf("Part 1: %d\n", handlePart1(input))
+	fmt.Printf("Part 2: %d\n", handlePart2(input))
+}
+
+func handlePart1(input string) int {
+	wordsearch := readInput(input)
+	allX := findXs(wordsearch)
+	return fp.Sum(fp.Map(allX, checkPoint(wordsearch, 0)))
+}
+
+func handlePart2(input string) int {
+	return 0
+}
+
+func readInput(input string) [][]rune {
+	return fp.Map(strings.Split(strings.TrimSpace(input), "\n"), func(s string) []rune { return []rune(s) })
+}
+
+func findXs(wordsearch [][]rune) []cartesian.Point {
+	result := []cartesian.Point{}
+
+	for y := range wordsearch {
+		for x := range wordsearch[y] {
+			if wordsearch[y][x] == 'X' {
+				result = append(result, cartesian.Point{X: x, Y: y})
+			}
+		}
+	}
+	return result
+}
+
+func checkPoint(wordsearch [][]rune, wordIdx int, direction ...cartesian.Direction) func(cartesian.Point) int {
+	return func(p cartesian.Point) int {
+		if !isInBounds(wordsearch, p) ||
+			wordsearch[p.Y][p.X] != wantWord[wordIdx] {
+			return 0
+		}
+
+		if wordIdx == len(wantWord)-1 {
+			return 1
+		}
+
+		if wordIdx == 0 {
+			toCheckDirections := [][]cartesian.Direction{
+				{cartesian.Left},
+				{cartesian.Left, cartesian.Up},
+				{cartesian.Up},
+				{cartesian.Right, cartesian.Up},
+				{cartesian.Right},
+				{cartesian.Right, cartesian.Down},
+				{cartesian.Down},
+				{cartesian.Left, cartesian.Down},
+			}
+
+			sum := 0
+
+			for _, direction := range toCheckDirections {
+				checker := checkPoint(wordsearch, wordIdx+1, direction...)
+				nextPoint := fp.Reduce(direction, p, func(curr cartesian.Point, next cartesian.Direction) cartesian.Point { return curr.Move(next) })
+
+				sum += checker(nextPoint)
+			}
+
+			return sum
+		}
+
+		checker := checkPoint(wordsearch, wordIdx+1, direction...)
+		nextPoint := fp.Reduce(direction, p, func(curr cartesian.Point, next cartesian.Direction) cartesian.Point { return curr.Move(next) })
+		return checker(nextPoint)
+	}
+}
+
+func isInBounds(wordsearch [][]rune, p cartesian.Point) bool {
+	return p.X >= 0 &&
+		p.Y >= 0 &&
+		p.X < len(wordsearch[0]) &&
+		p.Y < len(wordsearch)
+}
---
+++ day/04/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tsatam/adventofcode-2024/common/cartesian"
+)
+
+func TestHandlePart1(t *testing.T) {
+	input := `....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX`
+
+	want := 18
+	got := handlePart1(input)
+	assert.Equal(t, want, got)
+}
+
+func TestHandlePart2(t *testing.T) {
+	input := `.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........`
+
+	want := 18
+	got := handlePart1(input)
+	assert.Equal(t, want, got)
+}
+
+func TestFindXs(t *testing.T) {
+	wordsearch := readInput(`..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....`)
+	want := []cartesian.Point{
+		{X: 2, Y: 0},
+		{X: 4, Y: 1},
+		{X: 0, Y: 3},
+		{X: 1, Y: 4},
+	}
+	got := findXs(wordsearch)
+	assert.ElementsMatch(t, want, got)
+}
